9131f8d4f8379f58e274074e8740b7c2d96310d3
vinodsharma0202 <vinodsharma0202@gmail.com>
2016-08-27 19:28:41 +0200

    A very good start point for MVC spring learners

---
+++ practice-mongo/src/main/java/com/vinod/springmvc/controller/PracticeController.java
@@ -0,0 +1,33 @@
+package com.vinod.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+ 
+/**
+ * The Class PracticeController.
+ */
+@Controller
+public class PracticeController {
+	
+	/** The message. */
+	String message = "Welcome to Spring MVC!";
+ 
+	/**
+	 * Show message.
+	 *
+	 * @param name the name
+	 * @return the model and view
+	 */
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
